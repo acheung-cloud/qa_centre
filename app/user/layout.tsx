a11e63af5385706f464c1dc561ae36b3f54efb4f
Alvin Cheung <acheung@nstrlabs.com>
2025-02-09 10:16:54 +0800

    Middle of Server component

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,17 +1,35 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import UserHeader from "../components/UserHeader";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
-import UserLayout from "../components/UserLayout";
 
-Amplify.configure(outputs);
+Amplify.configure(outputs, { ssr: true });
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+function Header() {
+  const { user, signOut } = useAuthenticator();
+  return <UserHeader signOut={signOut} user={user} />;
+}
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <UserLayout>
-      {children}
-    </UserLayout>
+    <Authenticator>
+      <div className="flex h-full bg-gray-50">
+        <div className="flex flex-1 flex-col">
+          <Header />
+          <main className="flex-1 overflow-y-auto p-8">
+            <div className="mx-auto max-w-7xl">
+              {children}
+            </div>
+          </main>
+        </div>
+      </div>
+    </Authenticator>
   );
 }
